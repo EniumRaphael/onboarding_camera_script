297631e3145e7bd6d1e0d9be3207152ed2a06f7a
Raphael <rparodi@student.42.fr>
2025-07-20 00:51:41 +0200

    feat(setup/camera): Now enable the choiceof the camera

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import cv2 as cv
+import os
+import logging
+
+def list_cameras(max_tested=10):
+	available = []
+	for i in range(max_tested):
+		cap = cv.VideoCapture(i)
+		if cap is not None and cap.read()[0]:
+			logging.debug(f"Caméra détectée. (id => {i})")
+			available.append(i)
+		cap.release()
+	return available
+
+def choose_camera():
+	available = list_cameras()
+	if not available:
+		logging.critical("Aucune caméra détectée.")
+		exit(1)
+
+	print("Caméras disponibles :")
+	for idx in available:
+		print(f"[{idx}] /dev/video{idx} (ou équivalent)")
+
+	while True:
+		try:
+			choice = int(input("Choisissez le numéro de la caméra : "))
+			if choice in available:
+				return choice
+			else:
+				print("Numéro invalide.")
+		except ValueError:
+			print("Veuillez entrer un nombre.")
 
 def main():
-	cap = cv.VideoCapture(2) # change here the number if the wrong camera is used
+	cam_index = choose_camera()
+	cap = cv.VideoCapture(cam_index)
+
 
 	cap.set(cv.CAP_PROP_SETTINGS, 1)
 
 	width  = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 	height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-	print(f"{width=} / {height=}")
+	logging.info(f"Caméra initialisée avec une résolution de {width}x{height}.")
 
 	filename = ""
 	while True:
@@ -25,14 +59,14 @@ def main():
 		key = cv.waitKey(1)
 
 		if key == 0x1b:		# escape
-			print("Bisous bbye.")
+			logging.info(f"Caméra initialisée avec une résolution de {width}x{height}.")
 			break
 		elif key == 0x7f:	# backspace
 			filename = filename[:-1]
 		elif key == 0x0d:	# enter
 			filename = filename.replace(' ', '-')
 
-			print(f"Saving images/{filename}.jpg")
+			logging.info(f"Saving images/{filename}.jpg")
 
 			os.makedirs("images", exist_ok=True)
 			cv.imwrite(f"images/{filename}.jpg", img)
